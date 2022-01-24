70ae66c00452d9b53d8c4d989096c0177a2ef1b1
Wahab Syed <wahab+github@outlook.com>
2022-01-23 18:32:18 -0800

    updating index.js with new job title

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello, I'm <b>Wahab</b>. Currently, DevOps Cloud Architect at Amazon Web Services</p>
+        <p>Hello, I'm <b>Wahab</b>. Currently, Startup Solution Architect at Amazon Web Services</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
